f6737aae2d739fb32e8c01369e07a616b4ec6423
uprorock <uprorock@ya.ru>
2016-11-15 00:00:27 +0300

    Lab2, Rate This App dialog is done!

--- app/src/main/java/com/example/prorock/lab1/RateDialog.java
+++ app/src/main/java/com/example/prorock/lab1/RateDialog.java
@@ -2,6 +2,7 @@ package com.example.prorock.lab1;
 
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -10,6 +11,7 @@ import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -36,10 +38,11 @@ public class RateDialog extends android.support.v4.app.DialogFragment implements
 
     @Override
     public void onClick(View v) {
-        int id = ((Button)v).getId();
+        int id = v.getId();
         switch (id) {
             case R.id.button_rate:
-
+                rate_onClick();
+                //dismiss();
             break;
             case R.id.button_close:
                 dismiss();
@@ -48,5 +51,16 @@ public class RateDialog extends android.support.v4.app.DialogFragment implements
 
     }
 
+    private void rate_onClick()
+    {
+        Handler h = new Handler();
+        h.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(getContext(), R.string.toast_thanks, Toast.LENGTH_SHORT).show();
+                dismiss();
+            }
+        }, 1200);
+    }
 
 }
